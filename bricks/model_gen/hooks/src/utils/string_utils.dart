4852fa5b7dafea3218a5dd1357e635d809cdaf00
youssefmdev22 <youssefmdev22@gmail.com>
2025-10-28 20:02:45 +0300

    hotfix model_gen brick

--- bricks/model_gen/hooks/src/utils/string_utils.dart
+++ bricks/model_gen/hooks/src/utils/string_utils.dart
@@ -1,45 +1,37 @@
 class StringUtils {
   /// Converts string to camelCase
   /// Example: "user_name" -> "userName"
-  ///          "user name" -> "userName"
   static String toCamelCase(String input) {
-    input = input.trim().replaceAll(RegExp(r'^_+'), '');
-    final parts = input.split(RegExp(r'[_\s-]+'));
+    input = input.replaceAll(RegExp(r'^_+'), '');
+    final parts = input.split(RegExp(r'[_\s]+'));
 
     if (parts.isEmpty) return input;
 
-    return parts.first.toLowerCase() +
+    return parts.first +
         parts
             .skip(1)
             .map((p) => p.isNotEmpty
-            ? '${p[0].toUpperCase()}${p.substring(1).toLowerCase()}'
+            ? '${p[0].toUpperCase()}${p.substring(1)}'
             : '')
             .join();
   }
 
   /// Converts string to snake_case
   /// Example: "UserName" -> "user_name"
-  ///          "user name" -> "user_name"
-  ///          "user-Name" -> "user_name"
   static String toSnakeCase(String input) {
     return input
-        .trim()
         .replaceAllMapped(
       RegExp(r'([A-Z])'),
           (m) => '_${m.group(0)!.toLowerCase()}',
     )
-        .replaceAll(RegExp(r'[\s\-]+'), '_')
-        .replaceFirst(RegExp(r'^_+'), '')
+        .replaceFirst('_', '')
         .toLowerCase();
   }
 
   /// Converts string to PascalCase
   /// Example: "user_name" -> "UserName"
-  ///          "user name" -> "UserName"
-  ///          "user-name" -> "UserName"
   static String toPascalCase(String input) {
     return input
-        .trim()
         .split(RegExp(r'[_\s-]+'))
         .map((w) => w.isEmpty
         ? ''
@@ -50,9 +42,9 @@ class StringUtils {
   /// Cleans field name from leading underscores
   /// Example: "_userName" -> "userName"
   static String cleanFieldName(String key) {
-    key = key.trim();
     if (key.startsWith('_')) {
-      key = key.replaceFirst(RegExp(r'^_+'), '');
+      final clean = key.replaceFirst('_', '');
+      return toCamelCase(clean);
     }
     return toCamelCase(key);
   }
